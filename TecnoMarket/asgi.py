1890c53edb73e27329047090b8397a4e0aae21bc
Felix-Varela <pokermast394@hotmail.com>
2021-08-22 09:35:36 -0600

    Se agrega el proyecto

---
+++ TecnoMarket/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TecnoMarket project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TecnoMarket.settings')
+
+application = get_asgi_application()
